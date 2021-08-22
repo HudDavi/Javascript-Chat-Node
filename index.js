99cc47c3e3074ba67da62acec86abb041fcbf3e5
Hudson Davi <hudson.davi.cds@gmail.com>
2021-08-22 07:56:15 -0400

    Sistema de Chat com Node

---
+++ index.js
@@ -0,0 +1,18 @@
+const app = require('express')();
+const http = require('http').Server(app);
+const io = require('socket.io')(http);
+const port = process.env.PORT || 3000;
+
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/index.html');
+});
+
+io.on('connection', (socket) => {
+  socket.on('mensagens', dados => {
+    io.emit('mensagens', dados);
+  });
+});
+
+http.listen(port, () => {
+  console.log(`Servidor em Execução http://localhost:${port}/`);
+});
